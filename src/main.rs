47149fec4e3a0d75b717bb89cc6b4d8e035e2560
Namejess <jessy_drouin@protonmail.com>
2023-01-23 21:19:00 +0100

    ✨Add features + scss

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use yew::prelude::*;
+
+#[function_component(Producer)]
+fn producer() -> Html {
+    html! {
+
+        <div class={"producer"}>
+        <h3>{ "Producer" }</h3>
+        </div>
+    }
+}
+
+#[function_component(Consumer)]
+fn consumer() -> Html {
+    html! {
+
+        <div class="consumer">
+
+        <h3>{ "Consumer" }</h3>
+
+        </div>
+    }
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <div class={"grid"}>
+            <Producer/>
+            <Consumer/>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
 }
